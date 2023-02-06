da5ee43dd3da636906616041ebf3592063d79b4e
Afshrsgnd <124483567+Afshrsgnd@users.noreply.github.com>
2023-02-06 13:15:18 +0330

    Add files via upload

---
+++ day.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int A;
+
+	cout<<"Enter a Number : ";
+
+	cin>>A;
+
+	switch (A){
+
+		case 1:
+
+			cout<<"saturday . "<<endl;
+
+		break;
+
+		case 2:
+
+			cout<<"sunday . "<<endl;
+
+		break;
+
+		case 3:
+
+			cout<<"monday . "<<endl;
+
+		break;
+
+		case 4:
+
+			cout<<"tuesday"<<endl;
+
+		break;
+
+		case 5:
+
+		   cout<<"wednesday"<<endl;
+
+		break;
+
+		case 6:
+
+		  cout<<"thursday"<<endl;
+
+		break;
+
+		case 7:
+
+			cout<<"friday"<<endl;
+
+		break;
+
+    
+    default :
+
+     cout << " Undefined " << endl ;    
+
+}
+
+
+return 0;
+
+}
+
